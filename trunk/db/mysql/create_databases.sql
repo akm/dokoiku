e30739d1ca2b381e8214316820e85f6092d92621
akm2000 <akm2000@3ae1be9a-3f39-0410-b43e-85663bca58bb>
2007-09-16 05:47:27 +0000

    DBを生成するSQLを追加したので設定を修正しました。

--- trunk/db/mysql/create_databases.sql
+++ trunk/db/mysql/create_databases.sql
@@ -4,5 +4,3 @@ create database dokoiku_production default character set utf8;
 grant all on dokoiku_development.* to 'dokoiku_user'@'localhost';
 grant all on dokoiku_test.* to 'dokoiku_user'@'localhost';
 grant all on dokoiku_production.* to 'dokoiku_user'@'localhost';
-create database dokoiku_development_actual default character set utf8;
-grant all on dokoiku_development_actual.* to 'dokoiku_user'@'localhost';
