b1c9cab02c45b7ae4e425980acdbd40c3d467b64
akm2000 <akm2000@3ae1be9a-3f39-0410-b43e-85663bca58bb>
2007-09-18 12:22:48 +0000

    デバッグ用のGoogleMapAPIのキーを設定しました。

--- trunk/config/environment.rb
+++ trunk/config/environment.rb
@@ -66,5 +66,7 @@ ActionView::Base.field_error_proc = Proc.new{|html_tag, instance|
 
 # Include your application configuration below
 
-# GOOGLE_APPLICATION_ID = "aaaaa"
-GOOGLE_APPLICATION_ID = "ABQIAAAAnfs7bKE82qgb3Zc2YyS-oBT2yXp_ZAY8_ufC3CFXhHIE1NvwkxSySz_REpPq-4WZA27OwgbtyR3VcA"
+# http://localhost:3000 で実行する場合
+GOOGLE_APPLICATION_ID = "ABQIAAAAG1aAUyebKcmVH8kST6rXphTJQa0g3IQ9GZqIMmInSLzwtGDKaBQTo1Iom0Lb2v4tsvWzB-5g_Tr-yw"
+# http://localhost で実行する場合
+# GOOGLE_APPLICATION_ID = "ABQIAAAAnfs7bKE82qgb3Zc2YyS-oBT2yXp_ZAY8_ufC3CFXhHIE1NvwkxSySz_REpPq-4WZA27OwgbtyR3VcA"
