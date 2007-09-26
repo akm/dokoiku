79603c81f82a3865b526bb19a03bb1b468dea3bf
akm2000 <akm2000@3ae1be9a-3f39-0410-b43e-85663bca58bb>
2007-09-26 17:38:37 +0000

    IE7でGoogleMapの位置がずれてしまうのを無理やり直しました

--- trunk/public/javascripts/application.js
+++ trunk/public/javascripts/application.js
@@ -1,2 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+Browser = {
+	IE:     !!(window.attachEvent && !window.opera),
+	Opera:  !!window.opera,
+	WebKit: navigator.userAgent.indexOf('AppleWebKit/') > -1,
+	Gecko:  navigator.userAgent.indexOf('Gecko') > -1 && navigator.userAgent.indexOf('KHTML') == -1
+}
